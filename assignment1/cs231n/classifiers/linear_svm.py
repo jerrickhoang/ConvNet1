b713d203e41777edd580e32df3aea0638b686c95
jerrickhoang <jerrickhoang@gmail.com>
2015-01-10 23:18:18 -0500

    SVM: gradient implemented and checked

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -29,13 +29,19 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[j, :] += X[:, i].T
+        dW[y[i], :] += - X[:, i].T
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
+
+  # Add regularization to the gradient.
+  dW += reg * W
 
   #############################################################################
   # TODO:                                                                     #
